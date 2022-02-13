6e618a81f4d4321770dd3c6ed5e1609888ad81e5
Jacey Arianna Lowry <jacey.arianna@gmail.com>
2022-02-13 15:17:13 -0600

    commit tree 47bcbfc908abed220dcfd519b89220630ab3ee22

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 const init = () => {
-  
+//need event listener
+//even listener takes 2 parameters: type of event (`submit`) and callback function to handle the event
+  const inputForm = document.querySelector(`form`);
+
+  inputForm.addEventListener(`submit`, (e) => {
+    e.preventDefault() //prevent form refresh
+    const input = document.querySelector(`input#searchByID`)
+
+    fetch(`http://localhost:3000/movies/${input.value}`)
+    .then(resp => resp.json())
+    .then(data => {
+        const title = document.querySelector(`section#movieDetails h4`)
+        const summary = document.querySelector(`section#movieDetails p`)
+
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+    })
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
